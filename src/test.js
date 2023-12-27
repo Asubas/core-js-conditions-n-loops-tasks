0270a07a32140720c391629c3079e3296138dd62
unknown <znaxor74@mail.ru>
2023-12-27 23:15:18 +0300

    feat: add some test

--- src/test.js
+++ src/test.js
@@ -1,3 +1,13 @@
-const num = 1566;
+function isContainNumber(num, digit) {
+  const str = String(num);
+  const strDig = String(digit);
+  console.log(str);
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === strDig) {
+      return true;
+    }
+  }
+  return false;
+}
 
-console.log(num.toLocaleString());
+console.log(isContainNumber(123450, 5));
