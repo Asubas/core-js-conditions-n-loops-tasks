65ece8f582d0a17427203b52e9f6959b91ae46a7
Леха <znaxor74@mail.ru>
2023-12-28 16:34:49 +0300

    save commit

--- src/test.js
+++ src/test.js
@@ -1,28 +1,21 @@
 function rotateMatrix(matrix) {
-  const arr = matrix;
-  let startCol = 0;
-  let endCol = matrix.length - 1;
-  let startRow = 0;
-  let endRow = matrix.length - 1;
-  while (startCol <= endCol && startRow <= endRow) {
-    for (let i = startCol; i <= endCol; i += 1) {
-      arr[startRow][i] = arr[endRow][i];
+  const newMat = matrix;
+  for (let i = 0; i < newMat.length / 2; i += 1) {
+    for (let j = i; j < newMat.length - i - 1; j += 1) {
+      const temp = newMat[i][j];
+      // двигаю слева направо
+      newMat[i][j] = newMat[newMat.length - i - 1][j];
+
+      newMat[newMat.length - i - 1][j] =
+        newMat[newMat.length - j - 1][newMat.length - i - 1];
+
+      newMat[newMat.length - j - 1][newMat.length - i - 1] =
+        newMat[newMat.length - i - 1][newMat.length - j - 1];
+
+      newMat[newMat.length - i - 1][newMat.length - j - 1] = temp;
     }
-    startRow += 1;
-    for (let j = startRow; j <= endRow; j += 1) {
-      arr[j][endCol] = arr[j][startCol];
-    }
-    endCol -= 1;
-    for (let y = endCol; y >= startCol; y -= 1) {
-      arr[endRow][y] = arr[startCol][y];
-    }
-    endRow -= 1;
-    for (let u = endRow; u >= startRow; u -= 1) {
-      arr[u][startCol] = arr[u][endCol];
-    }
-    startCol += 1;
   }
-  return arr;
+  return matrix;
 }
 
 console.log(
